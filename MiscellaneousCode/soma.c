3713d894872317d508396b1bccedbd216bc4bc0d
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-20 21:38:03 -0300

    Update soma.c

--- MiscellaneousCode/soma.c
+++ MiscellaneousCode/soma.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main() {
+  
+  int n = 0;
+  int soma = 0;
+
+  while (soma < 100) {
+    printf("Please, enter the integer: ");
+    scanf("%d", &n);
+  }
+
+  printf("%d", &soma)
+  
+  return 0;
+}
